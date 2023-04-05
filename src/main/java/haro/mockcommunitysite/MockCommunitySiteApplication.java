283e728f2af437b902d0c7541a22524d7dcb2471
insightp25 <insightplug@gmail.com>
2023-04-05 19:47:22 +0900

    init commit

---
+++ src/main/java/haro/mockcommunitysite/MockCommunitySiteApplication.java
@@ -0,0 +1,13 @@
+package haro.mockcommunitysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockCommunitySiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockCommunitySiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haro/mockcommunitysite/MockCommunitySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package haro.mockcommunitysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockCommunitySiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
